d36cc2c8528e53e559a1a78732d2ec817bf4538a
tomitony123 <tomitony123@outlook.com>
2022-12-02 08:46:23 +0800

    test

--- src/Opt.swift
+++ src/Opt.swift
@@ -6,7 +6,7 @@ public struct Opt {
     // This is only used in finding the end of HTTP header (as of now). There is no limit on the length of http header, but Apache set it to 8KB
     public static var MAXNWTCPScanLength = 8912
 
-    public static var DNSFakeIPTTL = 300
+    public static var DNSFakeIPTTL = 128
 
     public static var DNSPendingSessionLifeTime = 10
 
